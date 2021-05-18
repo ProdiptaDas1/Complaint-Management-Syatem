d133c46a299d62171005ff80b467a4d7afe8ebd3
ProdiptaDas1 <83453339+ProdiptaDas1@users.noreply.github.com>
2021-05-18 20:52:47 +0600

    Add files via upload

---
+++ TestCode/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+             Thanks For Visit
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
